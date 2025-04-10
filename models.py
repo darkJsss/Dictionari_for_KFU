0958c3ac7680c6cf7a6a78485052262d32965e02
darkJs <damirfaz239@gmail.com>
2025-04-10 21:31:22 +0300

    version 1

--- models.py
+++ models.py
@@ -10,3 +10,13 @@ class BorrowedWord(db.Model):
 
     def __repr__(self):
         return f'<BorrowedWord {self.word}>'
+
+
+class Suggestion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(100), nullable=False)
+    russian_equivalent = db.Column(db.String(100))
+    origin = db.Column(db.String(200))
+    comment = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(20), default='pending')
